bf639fc23ace66d8388b1eceaefa4b9d4a1e2e9d
meaow-stack <126599725+meaow-stack@users.noreply.github.com>
2024-07-17 14:39:19 +0530

    Update App.js

--- App.js
+++ App.js
@@ -38,53 +38,55 @@ export default function App() {
     };
 
     return (
-        <>
-            <header>
-                <div className='App-header'>
-                </div>
-                <h1>The Weather App</h1>
-            </header>
-            <div className='container-fluid'>
-                <div className='card text-bg-info mb-3'>
-                    <MyComponent />
-                    <FaBeer />
+        <div className="body">
+            <>
+                <header>
+                    <div className='App-header'>
+                    </div>
+                    <h1>The Weather App</h1>
+                </header>
+                <div className='container-fluid'>
+                    <div className='card text-bg-info mb-3'>
+                        <MyComponent />
+                        <FaBeer />
+                    </div>
+
                 </div>
 
-            </div>
+                {error && (
+                    <div className="alert alert-danger mt-4" role="alert">
+                        {error}
+                    </div>
+                )}
 
-            {error && (
-                <div className="alert alert-danger mt-4" role="alert">
-                    {error}
-                </div>
-            )}
+                <MDBFooter bgColor="light" className="text-center text-lg-left mt-5">
+                    <MDBContainer className="p-4">
+                        <MDBRow>
+                            <MDBCol lg="6" md="12" className="mb-4 mb-md-0">
+                                <h5 className="text-uppercase">The Weather App</h5>
+                                <p>
+                                    Sayantan Mukherjee@Copyright
+                                </p>
+                            </MDBCol>
+                            <MDBCol lg="6" md="12" className="mb-4 mb-md-0">
+                                <h5 className="text-uppercase">For Errors email at
+                                    <a href="mailto:sayantanmukherjee@gmail.com"> Email</a>
+                                </h5>
+                                <p>
+                                    For Any query
+                                </p>
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBContainer>
 
-            <MDBFooter bgColor="light" className="text-center text-lg-left mt-5">
-                <MDBContainer className="p-4">
-                    <MDBRow>
-                        <MDBCol lg="6" md="12" className="mb-4 mb-md-0">
-                            <h5 className="text-uppercase">The Weather App</h5>
-                            <p>
-                                Sayantan Mukherjee@Copyright
-                            </p>
-                        </MDBCol>
-                        <MDBCol lg="6" md="12" className="mb-4 mb-md-0">
-                            <h5 className="text-uppercase">For Errors email at
-                                <a href="mailto:sayantanmukherjee@gmail.com"> Email</a>
-                            </h5>
-                            <p>
-                                For Any query
-                            </p>
-                        </MDBCol>
-                    </MDBRow>
-                </MDBContainer>
-
-                <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}>
-                    &copy; {new Date().getFullYear()} Copyright:{' '}
-                    <a className="text-dark" href="https://mdbootstrap.com/">
-                        MDBootstrap.com
-                    </a>
-                </div>
-            </MDBFooter>
-        </>
+                    <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}>
+                        &copy; {new Date().getFullYear()} Copyright:{'Sayantan'}
+                        <a className="text-dark" href="https://mdbootstrap.com/">
+                            MDBootstrap.com
+                        </a>
+                    </div>
+                </MDBFooter>
+            </>
+        </div>
     );
 }
